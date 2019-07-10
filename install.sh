67b546550d5e81d05d5786df164aaddde26f35f8
Jean-Louis Huynen <huynenjl@gmail.com>
2019-07-10 15:05:53 +0200

    chg: [install] only use virtualenv for compiling sage

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get install -y python3.7 python3.7-venv
-python3.7 -m venv VENV
-pip install -r requirements.txt
-. ./VENV/bin/activate
-wget http://www-ftp.lip6.fr/pub/math/sagemath/src/sage-8.8.tar.gz 
+sudo apt-get install -y python3.7 python3.7-venv redis-server
+sudo systemctl enable redis-server.service
+sudo systemctl restart redis-server.service
+wget http://www-ftp.lip6.fr/pub/math/sagemath/src/sage-8.8.tar.gz
 md5sum -c sagesum.txt
 if [$? = 1]; then
        	exit 1
@@ -12,7 +11,13 @@ fi
 sudo apt-get install build-essential m4 dpkg-dev
 tar -xvf sage-8.8.tar.gz
 pushd sage-8.8
+python3.7 -m venv VENV
+. ./VENV/bin/activate
 make configure
 ./configure --with-python=3
 make -j4 build
 sudo ln -sr sage /usr/local/bin/sage
+deactivate
+rm -rf VENV
+sage -i ipython
+sage -pip install -r requirements.txt
