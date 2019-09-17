983b89aa10c3627e4f39bee5b6705c07b9c502f8
Jean-Louis Huynen <huynenjl@gmail.com>
2019-09-17 11:22:53 +0200

    chg: [random] details

--- snake-oil-crypto.py
+++ snake-oil-crypto.py
@@ -7,8 +7,7 @@ import attacks
 def main():
     redis_conn = Redis()
     q = Queue(connection=redis_conn)
-    testChallenge(q)
-    # testDB(q)
+    testDB(q)
 
 
 def testChallenge(queue):
@@ -21,11 +20,11 @@ def testDB(queue):
     pg = datastores.PostGres()
     r = pg.getRSA()
     a = attacks.Tsage()
-    res = queue.enqueue(a.process, r.moduli)
-    match = [x for x in zip(r, res.return_value) if x[1] != 1]
-    print("Matching keys in DB: {0}".format(len(match)))
-    print(match)
-
+    res = queue.enqueue(a.process, r, job_timeout=15000)
+    queue.enqueue(a.report, res.id, depends_on=res, job_timeout=15000)
+#    match = [x for x in zip(r, res.return_value) if x[1] != 1]
+#    print("Matching keys in DB: {0}".format(len(match)))
+#    print(match)
 
 if __name__ == "__main__":
     main()
