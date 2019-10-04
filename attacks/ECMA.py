229d1b71c58ac611e627f6297587876878f1ed0c
Jean-Louis Huynen <huynenjl@gmail.com>
2019-10-04 11:52:04 +0200

    chg: [attacks] Sage ECM

---
+++ attacks/ECMA.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env sage -python
+from . import register_attack
+from . import Attack
+from rq import Queue, Connection
+from redis import Redis
+from sage.all import *
+
+@register_attack
+class Ecma(Attack):
+    def process(self, X):
+        f = ECM()
+        return(sorted(f.find_factor(X, None, 2000)[0]))
+
+    def report(self, processid):
+        with Connection(Redis()):
+            q = Queue()
+            res = q.fetch_job(processid)
+            print(res.return_value)
