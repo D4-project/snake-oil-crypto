01b5f22f0cc7dec9b31108e0febba67af77cfea8
Jean-Louis Huynen <huynenjl@gmail.com>
2019-10-17 16:30:54 +0200

    chg: [attacks] some debugging output

--- attacks/BatchGCD.py
+++ attacks/BatchGCD.py
@@ -5,6 +5,7 @@ import datastores
 from rq import Queue, Connection
 from redis import Redis
 from sage.all import *
+import hashlib
 
 @register_attack
 class BatchGCD(Attack):
@@ -16,19 +17,27 @@ class BatchGCD(Attack):
     def process(self, args, kwargs=None):
         """ process invoques args, a datastore function, with kwargs as its arguments,
          then proceed with performing the attack on the results """
+        m = hashlib.sha256()
         with self.ds:
             X = args() if kwargs is None else args(**kwargs)
+            # Added a hash to easily compare inputs between batches
+            m.update(str(X).encode('UTF-8'))
+            print("\033[92m"+"Attacking {0} keys - {1}.".format(len(X), m.hexdigest())+"\033[0m")
             return [X, self.batchgcd_faster(X)]
 
     def report(self, processid):
         with Connection(Redis()):
             q = Queue()
             res = q.fetch_job(processid)
-            # TODO investigate coprimes
+            # TODO investigate coprimes, what is the effec of sharing p AND q
             match = [x for x in zip(res.return_value[0], res.return_value[1]) if x[1] != 1 and x[0] != x[1]]
-            # print("From Reporting")
-            print(match)
-            print("Matching keys in TextFile: {0}".format(len(match)))
+            # match = [x for x in zip(res.return_value[0], res.return_value[1]) if x[1] != 1]
+            #  TODO move this code to an export to csv file method
+            # if len(match) > 1:
+            #     with open('output_text2/'+processid, 'w+') as rf:
+            #         for line in match:
+            #             rf.write("{0},{1}\n".format(line[0], line[1]))
+            print("\033[93m Factorized keys: {0} out of {1} \033[0m".format(len(match), format(len(res.return_value[0]))))
             return match
 
     def batchgcd_faster(self, X):
