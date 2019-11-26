628beeb7417401213d47e4cd0d013feb47bb9fb6
d4 <huynenjl@gmail.com>
2019-11-26 18:02:37 +0100

    chg: [install] sage fat binaries to handle other CPUs in KVM

--- install-19.04.sh
+++ install-19.04.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 set -u
+export SAGE_FAT_BINARY=yes
 sudo apt-get install -y python3 redis-server postgresql postgresql-server-dev-all
 sudo systemctl enable redis-server.service
 sudo systemctl restart redis-server.service
@@ -14,6 +15,6 @@ tar -xvf sage-8.9.tar.gz
 pushd sage-8.9
 make configure
 ./configure --with-python=3
-make build
+make  build
 sudo ln -sr sage /usr/local/bin/sage 
 sage -pip install redis cryptography rq psycopg2 factordb-pycli sqlachemy git+git://github.com/MISP/PyMISP
