f6bf1ed06acf536580e6c72aabef83974bb6f2fd
Jean-Louis Huynen <huynenjl@gmail.com>
2020-11-18 10:53:20 +0100

    add: [ubuntu] fix

--- install-20.04.sh
+++ install-20.04.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#set -e
-#set -u
-#sudo apt-get install -y python3 redis-server postgresql postgresql-server-dev-all
-#sudo ln -s /usr/bin/python3 /usr/bin/python
-#sudo systemctl enable redis-server.service
-#sudo systemctl restart redis-server.service
-#wget http://www-ftp.lip6.fr/pub/math/sagemath/linux/64bit/sage-9.2-Ubuntu_20.04-x86_64.tar.bz2
-#md5sum -c sagesum9.2.txt
-#if [$? = 1]; then
+set -e
+set -u
+sudo apt-get install -y python3 redis-server postgresql postgresql-server-dev-all
+sudo ln -s /usr/bin/python3 /usr/bin/python
+sudo systemctl enable redis-server.service
+sudo systemctl restart redis-server.service
+wget http://www-ftp.lip6.fr/pub/math/sagemath/linux/64bit/sage-9.2-Ubuntu_20.04-x86_64.tar.bz2
+md5sum -c sagesum9.2.txt
+if [$? = 1]; then
        	#exit 1
-#fi
-#tar -xvf sage-9.2-Ubuntu_20.04-x86_64.tar.bz2
-#sudo ln -sr SageMath/sage /usr/local/bin/sage 
+fi
+tar -xvf sage-9.2-Ubuntu_20.04-x86_64.tar.bz2
+sudo ln -sr SageMath/sage /usr/local/bin/sage 
 sage -pip install redis cryptography rq psycopg2 factordb-pycli SQLAlchemy git+git://github.com/MISP/PyMISP
